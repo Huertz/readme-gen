3f354645f3fb3eb2e7296f20a58032ae247bb4a5
Huertz <104065687+Huertz@users.noreply.github.com>
2022-10-01 02:11:16 -0500

    Added inquirer.promt

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,74 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+// const { inherits } = require('util');
+const {writeFile} = require('fs').promises;
+const {generateReadme} = require('./utils')
 // TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const init = async() => {
+    try {
+ const preguntas = await inquirer
 
-// TODO: Create a function to initialize app
-function init() {}
+inquirer.prompt ( [
 
+    {
+        
+        type: 'input',
+        name: 'userName',
+        message: 'Enter Github User Name'
+    },
+
+    {
+        
+        type: 'input',
+        name: 'email',
+        message: 'Enter Email Address'
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter Project Title'
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter Project Description'
+    },
+    
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose Type Of License',
+        choices: ['MIT License', 'None', ]
+    },
+    
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter Instalation Instructions'
+    },
+    
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter Usage'
+    },
+    
+]);
+// // TODO: Create a function to write README file
+ await writeFile('README', generateReadme(preguntas));
+console.log('File Was Created')
+}catch(e){ 
+    console.log(e)
+}
+}
 // Function call to initialize app
 init();
+
+
+
+
+
+

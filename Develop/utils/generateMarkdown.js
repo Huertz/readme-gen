d0687548fc82fca5f4a7a1711f4778d8cbaa82fa
Huertz <104065687+Huertz@users.noreply.github.com>
2022-10-01 16:11:50 -0500

    Runs correctly need to add badge sec

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,18 +8,22 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown({title, instructions, license, userName, description, usage, test, emial, contribution}) {
+  return `# ${title}
   # <Your-Project-Title>
 
   ## Description
+
+  Short description of the project: 
+
+  ${description}
   
   ## Table of Contents
-  
-  If your README is long, add a table of contents to make it easy for users to find what they need.
   
   - [Installation](#installation)
   - [Usage](#usage)
@@ -27,19 +31,34 @@ function generateMarkdown(data) {
   - [License](#license)
   
   ## Installation
-  
+
+  To install necessary dependencies, run the following command: 
+
+  ${instructions} 
+
   ## Usage
 
-  ## Credits
+  ${usage}
   
   ## License
-  
-  ## Contributing
+
+  This project is under the ${license} 
+
+  ## Contributing 
+
+  ${contribution}
   
   ## Tests
 
+  To run the code, run the follwing command:
+
+  ${test}
+
   ## Questions
-  
+
+  If you want to view more projects. Your are welcome to visit me at https://github.com/${userName}
+
+  If you have addition question you can email me at ${emial}@yahoo.com 
 `;
 }
 
